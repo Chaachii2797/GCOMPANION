40b0eaa109c02defa2ae0d6277f8da604cce9727
Daphny Cabiso <daphnycabiso@bsit.usjr.edu.ph>
2017-07-25 09:24:44 +0800

    DIY static infos

---
+++ app/src/main/java/cabiso/daphny/com/gcompanion/HomePicture2.java
@@ -0,0 +1,17 @@
+package cabiso.daphny.com.gcompanion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Lenovo on 7/24/2017.
+ */
+
+public class HomePicture2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_picture2);
+    }
+}
